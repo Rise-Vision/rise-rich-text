7c0293bb934f9b0020bdc39acdc46f1d41a61cd5
Oleg Khasimkhanov <olegkhasimkhanov@Olegs-MacBook-Pro.local>
2020-07-06 16:04:20 -0400

    Load google fonts

--- src/rise-rich-text.js
+++ src/rise-rich-text.js
@@ -13,6 +13,10 @@ export default class RiseRichText extends RiseElement {
       richtext: {
         type: String,
         observer: "_richTextChanged"
+      },
+      googlefonts: {
+        type: Array,
+        observer: "_googleFontsChanged"
       }
     };
   }
@@ -21,12 +25,23 @@ export default class RiseRichText extends RiseElement {
     super();
 
     this._setVersion( version );
-    this.validFont = false;
+
+    //create empty link element so it can be re-used when googlefonts change
+    this.googleFontsLink = document.createElement("link");
+    this.googleFontsLink.rel  = "stylesheet";
+    this.googleFontsLink.type = "text/css";
+    document.head.appendChild(this.googleFontsLink);
   }
 
   _richTextChanged(newValue, oldValue) {
     this._refresh();
     super._sendEvent( RiseRichText.EVENT_DATA_UPDATE, {newValue, oldValue});
+  }
+
+  _googleFontsChanged() {
+    if (this.googlefonts && this.googlefonts.length) {
+      this.googleFontsLink.href =  "https://fonts.googleapis.com/css2?display=swap&family=" + encodeURI(this.googlefonts.join("&family="));
+    }
   }
 
   _refresh() {
